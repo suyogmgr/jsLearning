226811678b28d8912ec825b53a140a01afc27c4c
suyog <mgrsuyog6@gmail.com>
2024-02-26 20:22:05 +0545

    newProjects

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // let username;
 
 // document.getElementById("myBtn").onclick = () =>{
@@ -17,15 +15,63 @@
 
 // console.log(age);
 
+// const PI = 3.14;
+// let radius;
+// let circum;
 
-const PI = 3.14;
-let radius;
-let circum;
+// document.getElementById("myBtn").onclick = () => {
 
-document.getElementById("myBtn").onclick = () => {
+//     radius = document.getElementById("myTxt").value;
+//     radius = Number(radius);
+//     circum = 2 * PI * radius;
+//     document.getElementById("answer").textContent = circum + "cm";
+// }
 
-    radius = document.getElementById("myTxt").value;
-    radius = Number(radius);
-    circum = 2 * PI * radius;
-    document.getElementById("answer").textContent = circum;
-}
+//Number add,reset and decrease
+
+// const decreaseBtn = document.getElementById("decrease");
+// const resetBtn = document.getElementById("reset");
+// const increaseBtn = document.getElementById("increase");
+// const displayLabel = document.getElementById("display");
+// let count = 0;
+
+// increaseBtn.onclick = function(){
+//     count++;
+//     displayLabel.textContent = count;
+// }
+
+// decreaseBtn.onclick = function(){
+//     count--;
+//     displayLabel.textContent = count;
+// }
+
+// resetBtn.onclick = function(){
+//     count = 0;
+//     displayLabel.textContent = count;
+// }
+
+const checkBox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visa");
+const debitBtn = document.getElementById("debit");
+const paypalBtn = document.getElementById("paypal");
+const submitBtn = document.getElementById("myBtn");
+const checkBoxRes = document.getElementById("checkResult");
+const checkRadioRes = document.getElementById("radioResult");
+
+submitBtn.onclick = function () {
+  if (checkBox.checked) {
+    checkBoxRes.textContent = `Third Project Selected`;
+  } else {
+    checkBoxRes.textContent = `Third Project Not Selected`;
+  }
+
+  if (visaBtn.checked) {
+    checkRadioRes.textContent = `You have selected Visa`;
+  } else if (debitBtn.checked) {
+    checkRadioRes.textContent = `You have selected Debit`;
+  } else if (paypalBtn.checked) {
+    checkRadioRes.textContent = `You have selected Paypal`;
+  } else {
+    checkRadioRes.textContent = `You haven't selected anything`;
+  }
+};
