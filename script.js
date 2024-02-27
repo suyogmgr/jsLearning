b8f85c4e638b1c2120e51d30e8cbba8d521ac436
suyog <mgrsuyog6@gmail.com>
2024-02-27 18:29:39 +0545

    updates

--- script.js
+++ script.js
@@ -50,28 +50,63 @@
 //     displayLabel.textContent = count;
 // }
 
-const checkBox = document.getElementById("myCheckbox");
-const visaBtn = document.getElementById("visa");
-const debitBtn = document.getElementById("debit");
-const paypalBtn = document.getElementById("paypal");
-const submitBtn = document.getElementById("myBtn");
-const checkBoxRes = document.getElementById("checkResult");
-const checkRadioRes = document.getElementById("radioResult");
+// const checkBox = document.getElementById("myCheckbox");
+// const visaBtn = document.getElementById("visa");
+// const debitBtn = document.getElementById("debit");
+// const paypalBtn = document.getElementById("paypal");
+// const submitBtn = document.getElementById("myBtn");
+// const checkBoxRes = document.getElementById("checkResult");
+// const checkRadioRes = document.getElementById("radioResult");
 
-submitBtn.onclick = function () {
-  if (checkBox.checked) {
-    checkBoxRes.textContent = `Third Project Selected`;
-  } else {
-    checkBoxRes.textContent = `Third Project Not Selected`;
-  }
+// submitBtn.onclick = function () {
+//   if (checkBox.checked) {
+//     checkBoxRes.textContent = `Third Project Selected`;
+//   } else {
+//     checkBoxRes.textContent = `Third Project Not Selected`;
+//   }
 
-  if (visaBtn.checked) {
-    checkRadioRes.textContent = `You have selected Visa`;
-  } else if (debitBtn.checked) {
-    checkRadioRes.textContent = `You have selected Debit`;
-  } else if (paypalBtn.checked) {
-    checkRadioRes.textContent = `You have selected Paypal`;
-  } else {
-    checkRadioRes.textContent = `You haven't selected anything`;
-  }
-};
+//   if (visaBtn.checked) {
+//     checkRadioRes.textContent = `You have selected Visa`;
+//   } else if (debitBtn.checked) {
+//     checkRadioRes.textContent = `You have selected Debit`;
+//   } else if (paypalBtn.checked) {
+//     checkRadioRes.textContent = `You have selected Paypal`;
+//   } else {
+//     checkRadioRes.textContent = `You haven't selected anything`;
+//   }
+// };
+
+// -------------- SLICING ------------------
+// let email = "suyogido@gmail.com";
+
+// let username = email.slice(0, email.indexOf("@"));
+// let extension = email.slice(email.indexOf("@")+1);
+
+// console.log(username);
+// console.log(extension);
+
+// -------------- METHOD CHAINING ------------------
+
+//first without method chaining
+
+ let username = window.prompt("Enter your user name:");
+
+// username = username.trim(); //to remove the whitespace
+
+// let letter = username.charAt(0);
+
+// letter = letter.toUpperCase();
+
+// let remaning = username.slice(1);
+
+// remaning = remaning.toLowerCase();
+// let all = letter + remaning;
+// console.log(all);
+
+// ------- With Method Chaining ----------
+
+username =
+  username.trim().charAt(0).toUpperCase() +
+  username.trim().slice(1).toLowerCase();
+
+console.log(username);
